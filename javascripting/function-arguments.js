058b09c3984def35000f4c9c2c6947a4753de5ee
Brad Williams <bradley_williams2003@hotmail.com>
2022-10-18 12:04:55 -0300

    added the rest of javascripting lessons

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+function math(arg1, arg2, arg3) {
+  let multi = arg2 * arg3;
+  return arg1 + multi;
+}
+
+console.log(math(53, 61, 67));
---
+++ javascripting/object-keys.js
@@ -0,0 +1,9 @@
+const truck = {
+  make: 'Dodge',
+  model: 'Ram',
+  year: 2016
+}
+
+const keys = Object.keys(truck);
+
+console.log(keys);
